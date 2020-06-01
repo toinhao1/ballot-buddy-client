95f5b305563300d98fa205e02dc51432336686ab
toinhao1 <41380451+toinhao1@users.noreply.github.com>
2020-06-01 11:21:45 +0200

    This time

--- components/Card/index.js
+++ components/Card/index.js
@@ -3,6 +3,7 @@ import { View } from 'react-native';
 
 import { styles } from './Styles';
 
+// Basic card component so I cna control all the styling
 const Card = (props) => {
 	return (
 		<View style={{ ...styles.card, ...props.style }}>{props.children}</View>
